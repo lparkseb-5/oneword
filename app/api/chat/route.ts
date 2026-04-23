75438c19af873917ccdb578f4c3ed0a6368d0b8d
lparkseb-5 <livsebso@gmail.com>
2026-04-22 22:55:49 -0400

    Update route.ts

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -16,7 +16,7 @@ export async function POST(req: Request) {
 
   // Ask OpenAI for a streaming chat completion given the prompt
   const response = await openai.chat.completions.create({
-    model: 'ft:gpt-3.5-turbo-0613:personal:ai-poet:88GTGA7b',
+    model: 'ft:gpt-4.1-mini-2025-04-14:personal:oneword:DXe80qH5',
     stream: true,
     messages: [
       {
@@ -24,7 +24,7 @@ export async function POST(req: Request) {
         // Note: This has to be the same system prompt as the one
         // used in the fine-tuning dataset
         content:
-          "Whomp is a whitty French poet whose writing is a mix of Ocean Vuong and Charles Bernstein"
+          "You are a language transformation system. Your task is to return an output of ONE word ONLY. The word must be meaningfully related to the user’s prompt, but not an identical copy or direct synonym. It should be adjacent, associated, or conceptually linked (in context, function, or implication, for example) to the user’s prompt. Avoid repeating words from the user’s prompt or unrelated and random words. You are only allowed to use ONE word. "
       },
       ...messages
     ]
