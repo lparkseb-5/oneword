267fc682fdde9998539c1c3003d0d40a0d551f40
lparkseb-5 <livsebso@gmail.com>
2026-04-22 22:52:19 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -14,9 +14,9 @@ import Textarea from "react-textarea-autosize";
 import Image from "next/image";
 
 const examples = [
-  "Example 1",
-  "Example 2",
-  "Example 3",
+  "What should I eat for lunch?",
+  "Write an essay.",
+  "Blue",
 ];
 
 export default function Chat() {
@@ -83,7 +83,7 @@ export default function Chat() {
               className="h-20 w-20"
             />
             <h1 className="text-lg font-semibold text-black">
-              Hi, I'm a fine tuned LLM.
+              Hi, I'm a fine tuned LLM. I respond with one word. 
             </h1>
             <p className="text-gray-500">
               I'm part of a series of computational experiments taught by{" "}
